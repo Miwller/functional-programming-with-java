bc310ae0d1dfa0c8c71c3a1f0ed7ac8db8b718e4
Miwller <cloud.enginner.salvador@outlook.com>
2025-07-01 22:25:59 -0500

    Introduction to Functional Programming with Java

---
+++ src/main/java/com/programming/FP01Exercises.java
@@ -0,0 +1,66 @@
+package com.programming;
+
+import java.util.List;
+
+public class FP01Exercises {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+
+        //printOddNumbersInListFunctional(numbers);
+        //printSquaresOfEvenNumbersInListFunctional(numbers);
+        printCubesOfOddNumbersInListFunctional(numbers);
+
+        List<String> courses =
+                List.of("Spring", "Spring Boot", "API", "Microservices",
+                        "AWS", "PCF", "Azure", "Docker", "Kubernetes");
+
+        //courses.forEach(System.out::println);
+
+        /*courses.stream()
+                .filter(course -> course.contains("Spring"))
+                .forEach(System.out::println);*/
+
+        /*courses.stream()
+                .filter(course -> course.length() >= 4)
+                .forEach(System.out::println);*/
+
+        courses.stream()
+                .map(course -> course + ": " + course.length())
+                .forEach(System.out::println);
+
+    }
+
+    private static void printOddNumbersInListFunctional(List<Integer> numbers) {
+
+        //What to do?
+        numbers.stream()
+                //.filter(FP01Functional::isEven)
+                .filter(number -> number % 2 != 0)
+                .forEach(System.out::println); //Method Reference
+
+    }
+
+    private static void printSquaresOfEvenNumbersInListFunctional(List<Integer> numbers) {
+
+        //What to do?
+        numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .map(number -> number * number)
+                .forEach(System.out::println); //Method Reference
+
+    }
+
+    private static void printCubesOfOddNumbersInListFunctional(List<Integer> numbers) {
+
+        //What to do?
+        numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .map(number -> number * number * number)
+                .forEach(System.out::println); //Method Reference
+
+    }
+
+}
+
